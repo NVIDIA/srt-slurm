86a26cb8794fe1ec0fbebaf5892cd2626248e8bf
Sahithi Chigurupati <chigurupati.sahithi@gmail.com>
2026-04-08 12:35:52 -0700

    add copyright header to plot_batch_metrics script

--- src/srtctl/benchmarks/scripts/plot_batch_metrics.py
+++ src/srtctl/benchmarks/scripts/plot_batch_metrics.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Plot prefill/decode batch metrics over time from SGLang worker logs.
 
