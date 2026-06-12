33d68c78580567f404b6f107459920dd676d1d2b
richardhuo-nv <rihuo@nvidia.com>
2026-06-12 15:34:40 -0700

    fix

---
+++ configs/setup_nixl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+set -eux
+
+ln -sfn /usr/local/lib/python3.12/dist-packages/nixl_cu13 /usr/local/lib/python3.12/dist-packages/nixl
