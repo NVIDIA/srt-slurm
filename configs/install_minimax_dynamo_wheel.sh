267934d8f3963476de68ad59cea2988a5f288c87
richardhuo-nv <rihuo@nvidia.com>
2026-06-13 20:31:55 -0700

    fix

---
+++ configs/install_minimax_dynamo_wheel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+set -eux
+
+pip install --break-system-packages --force-reinstall /wheels/*.whl
